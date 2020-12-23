8da3b5a9b2c61cbd8526f25d21ca94b2223e5fed
I Komang Sena <i.buwana@btpn.com>
2020-12-23 15:27:02 +0700

    Add prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,46 @@
 const Generator = require('yeoman-generator');
 
 class MicroserviceGenerator extends Generator {
-  constructor(args, opts) {
-    super(args, opts);
+  initializing() {
+    this.pkg = this.fs.readJSON(this.destinationPath('package.json'), {});
 
-    this.log('hello world');
+    this.defaultProps = {
+      name: this.pkg.name || this.appname,
+      description: this.pkg.description || '',
+      version: this.pkg.version || '1.0.0',
+      engines: {
+        node: this.pkg.engines.node || 14,
+      },
+    };
+  }
+
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        name: 'name',
+        type: 'input',
+        default: this.defaultProps.name,
+        message: 'What is the name of your microservice?',
+      },
+      {
+        name: 'node',
+        type: 'input',
+        default: this.defaultProps.engines.node,
+        message: 'What version of node will you be using?',
+      },
+      {
+        name: 'healthcheck',
+        type: 'confirm',
+        default: true,
+        message: 'Would you like to to generate a health check route?',
+      },
+      {
+        name: 'postgres',
+        type: 'confirm',
+        default: false,
+        message: 'Would you like to to initialize postgres database?',
+      },
+    ]);
   }
 }
 
